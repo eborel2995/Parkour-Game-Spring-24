6a630e9facdc13ec8954f718a5d646e0e9f5e2cd
MagicalMongoose <drew1681@gmail.com>
2024-03-10 12:27:55 -0500

    Added Health and Bits scripts

---
+++ Assets/Scripts/Player/Bits.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bits : MonoBehaviour
+{
+    [SerializeField] private int bitCount = 0;
+    public int BitCount { get { return bitCount; } set { bitCount = value; } }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
