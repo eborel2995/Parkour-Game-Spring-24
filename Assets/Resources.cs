229860011bbe9429f5aa5992eae2441f46d5893b
MagicalMongoose <drew1681@gmail.com>
2024-03-12 13:07:53 -0500

    Added resources (bit count and health)

---
+++ Assets/Resources.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resources : MonoBehaviour
+{
+    [SerializeField] private int bitCount = 0;
+    [SerializeField] private int health = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
