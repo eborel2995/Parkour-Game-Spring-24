2029a1f87d6236f3c97cf1e217916997d4677467
curtlandmcdonald <curtlandm@yahoo.com>
2024-03-11 22:13:45 -0500

    Reworked player sprite.
    Reworked the player sprite so i could put on the running and idle animations.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;         // Variable to hold our 2D rigid body (player). 
+    private Animator anim;          // Variable to hold the animator component.
+    private SpriteRenderer sprite;  // Variable to hold the sprite renderer component.
     private float dirX = 0f;        // Variable to hold direction on the x-axis (initialized to zero).
 
     // "[SerializeFeild]" allows these variables to be edited in Unity.
@@ -16,6 +18,12 @@ public class PlayerMovement : MonoBehaviour
     {
         // Store GetComponent<Rigidbody2D>() once in rb to save memory and CPU resources.
         rb = GetComponent<Rigidbody2D>();
+
+        // Store GetComponent<Animator>() once in anim to save memory and CPU resources.
+        anim = GetComponent<Animator>();
+
+        // Store GetComponent<SpriteRenderer>() once in sprite to save memory and CPU resources.
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame.
@@ -36,6 +44,31 @@ public class PlayerMovement : MonoBehaviour
 
             // Velocity for each axis: x = velocity from previous movement and y = 14.
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+
+        // Call UpdateAnimationState() method.
+        UpdateAnimationState();
+    }
+
+    // Method to update animation states.
+    private void UpdateAnimationState()
+    {
+        // If moving right (positive x-axis) set running animation to true.
+        if (dirX > 0f)
+        {
+            anim.SetBool("running", true);  // running animation = true
+            sprite.flipX = false;           // flip animation to face right
+        }
+        // If moving left (negative x-axis) set running animation to true and flip animation on the x-axis.
+        else if (dirX < 0f)
+        {
+            anim.SetBool("running", true);  // running animation = true
+            sprite.flipX = true;            // flip animatino to face left
+        }
+        // If not moving set running animation to false.
+        else
+        {
+            anim.SetBool("running", false); // running animation = false
         }
     }
 }
