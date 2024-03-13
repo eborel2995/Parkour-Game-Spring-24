23c0622fa105ddbb704ed70dbf6ce1ade7781c6b
MagicalMongoose <drew1681@gmail.com>
2024-03-13 17:55:56 -0500

    Implemented simple timer and moved into Scripts folder

--- Assets/Scripts/AntiVirusScript.cs
+++ Assets/Scripts/AntiVirusScript.cs
@@ -1,9 +1,13 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class AntiVirusScript : MonoBehaviour
 {
+    private float countdownRemaining = 10;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,14 @@ public class AntiVirusScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (countdownRemaining > 0) 
+        {
+            countdownRemaining -= Time.deltaTime;
+        }
+        else
+        {
+            //Debug.Log("ANTIVIRUS SCAN ACTIVATED");
+            countdownRemaining = 10;
+        }
     }
 }
