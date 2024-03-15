5d7932a3d7d0795fa9234aa327da5c782e1a8276
MagicalMongoose <drew1681@gmail.com>
2024-03-14 21:50:55 -0500

    Added level gameplay requirements prefab

--- Assets/Scripts/Cheats.cs
+++ Assets/Scripts/Cheats.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Cheats : MonoBehaviour
 {
+    [SerializeField] Vector3 resize;
+    [SerializeField] float gravity;
+
+    Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        gravity = rb.gravityScale;
     }
 
     // Update is called once per frame
     void Update()
     {
+        transform.localScale = resize;
+        rb.gravityScale = gravity;
         
+        //fix issue of losing rb when scene changes!
+
     }
 }
