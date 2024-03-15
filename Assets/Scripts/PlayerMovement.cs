e265bd29c4a686207f9c84f4c9996fa9036feb4a
MagicalMongoose <drew1681@gmail.com>
2024-03-15 18:33:22 -0500

    Attempted player movement upgrades
    only got max fall speed

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,9 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Security.Cryptography;
 using Unity.VisualScripting.ReorderableList;
 using UnityEngine;
 using UnityEngine.InputSystem.Controls;
+using UnityEngine.ProBuilder.MeshOperations;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -23,6 +25,26 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float moveSpeed;          
     [SerializeField] private float jumpForce;         
     [SerializeField] private LayerMask jumpableGround;      // Variable to check against IsGrounded() method.
+
+    /*
+    // TODO: higher jump when holding jump longer
+    private bool endedJumpEarly = true;
+    private float currentTimeHoldingJump = 0f;
+    private float fallSpeed = 5;
+    private float jumpEndEarlyGravityModifer = 20;
+    */
+
+    //jump buffer
+    //private float jumpBuffer = 0.1f;
+
+    //coyote time
+    /*
+    private float timeLeftGround;
+    private float coyoteTimeThreshhold = 0.1f;
+    */
+
+    //clamped fall speed
+    [SerializeField] private float maxFallSpeed = -5;
 
     // Enum of movement state animations for our player to cycle through.
     // Each variable equals      0     1        2        3        mathematically.
@@ -89,19 +111,21 @@ public class PlayerMovement : MonoBehaviour
         // Use dirX to create velocity on the x-axis (joy-stick compatible).
         //rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
 
-        // If space is pressed and IsGround() method returns true, then player jumps.
 
-        //how is "Jump" defined as space??? -Drew
-        if (Input.GetButtonDown("Jump") && IsGrounded())
+        //jump input buffer 
+        /*
+        if (IsGrounded() && lastJumpPressed + jumpBuffer > Time.time)
         {
-            // Play the jump sound effect.
-            //jumpSoundEffect.Play();
+            jump();
+        }
+        */
 
-            //using AddForce causes an unintended effect of being able to climb walls by spamming jump on the wall
-            //but its kinda fun and replicable so I'm gonna keep it for now
-            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-
-            //rb.velocity = new Vector2(rb.velocity.x, jumpForce); //old jump code
+        //if player is holding jump and on ground, then jump
+        jump();
+        
+        if (rb.velocity.y < maxFallSpeed)
+        {
+            rb.velocity = new Vector3(rb.velocity.x, maxFallSpeed);
         }
 
         UpdateAnimationState();
@@ -145,6 +169,7 @@ public class PlayerMovement : MonoBehaviour
         anim.SetInteger("state", (int)state);
     }
     
+
     /// <summary>
     /// Create a box around the player model that is slightly lower than player's hitbox.
     /// </summary>
@@ -155,5 +180,37 @@ public class PlayerMovement : MonoBehaviour
     private bool IsGrounded()
     {
         return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, 0.1f, jumpableGround);
+    }
+
+    private void jump()
+    {
+        //DateTime now = DateTime.Now;
+        // If space is pressed and IsGround() method returns true, then player jumps.
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            //jumpSoundEffect.Play();
+
+
+            //using AddForce causes an unintended effect of being able to climb walls by spamming jump on the wall
+            //but its kinda fun and replicable so I'm gonna keep it for now
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+
+            //rb.velocity = new Vector2(rb.velocity.x, jumpForce); //old jump code
+        }
+
+        //if (Input.GetButtonUp("Jump") && ??? && rb.velocity.y > 0)
+        //{
+        //endedJumpEarly = true;
+
+
+        //}
+
+        /*
+        endedJumpEarly && rb.velocity.y > 0
+            ? fallSpeed * jumpEndEarlyGravityModifer
+            : fallSpeed;
+        
+        rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y - (fallSpeed * Time.deltaTime));
+        */
     }
 }
