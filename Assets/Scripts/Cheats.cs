09614b9a227c3e5c6f6ba88e7fdd52d3375e40d8
MagicalMongoose <drew1681@gmail.com>
2024-03-26 13:39:08 -0500

    Hooked cheats into mouseControl and added resizing

--- Assets/Scripts/Cheats.cs
+++ Assets/Scripts/Cheats.cs
@@ -5,8 +5,9 @@ using UnityEngine.UIElements;
 
 public class Cheats : MonoBehaviour
 {
-    [SerializeField] Vector3 resize;
+    [SerializeField] float resize = 1;
     [SerializeField] float gravity;
+    [SerializeField] MouseControl mouseControl;
 
     Rigidbody2D rb;
 
@@ -15,18 +16,26 @@ public class Cheats : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         gravity = rb.gravityScale;
+        mouseControl = GetComponent<MouseControl>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.localScale = resize;
+        //transform.localScale = resize;
         rb.gravityScale = gravity;
 
 
-        if (Input.GetKeyDown(KeyCode.C))
+        if (Input.GetKeyDown(KeyCode.R))
         {
-            
+            Debug.Log("R pressed");
+            //(toggle) player.Equip(resizer);
+
+        }
+
+        if (mouseControl.selectedObject != null) //player.currentlyEquiped(resizer) == true
+        {
+            mouseControl.selectedObject.transform.localScale = new Vector3(resize, resize, 1);
         }
         
         //fix issue of losing rb when scene changes!
