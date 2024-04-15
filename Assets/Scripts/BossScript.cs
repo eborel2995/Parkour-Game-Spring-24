8a8869271f17e9813b9c7776e1b3afba6322ef3a
curtlandmcdonald <curtlandm@yahoo.com>
2024-04-15 16:31:05 -0500

    updated boss arena and started making boss.

---
+++ Assets/Scripts/BossScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossScript : MonoBehaviour
+{
+    // For Idle Stage
+    [Header("Idle")]
+    [SerializeField] float idleMoveSpeed;
+    [SerializeField] Vector2 idleMoveDirection;
+    // For Attack Up and Down Stage
+    // For Attack Player Stage
+    // Other
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
