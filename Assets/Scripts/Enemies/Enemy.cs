ca5a0ff5028747801e86c0f214233c88a8fb88f7
SaltyJones79 <jeremy.saltz@gmail.com>
2024-04-15 17:57:31 -0500

    Saving what I have

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] protected float health;
+    [SerializeField] protected float recoilLength;
+    [SerializeField] protected float recoilFactor;
+    [SerializeField] protected bool isRecoiling = false;
 
     protected PlayerMovement player;
     [SerializeField] protected float moveSpeed = 5f;
-    
+
+    [SerializeField] protected float damage;
+
     protected Rigidbody2D rb;
     protected SpriteRenderer sr;
     protected Animator anim;
-    
 
+    protected float recoilTimer;
     // Start is called before the first frame update
     protected virtual void Start()
     {
@@ -28,7 +34,41 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     protected virtual void Update()
     {
-        
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+        if (isRecoiling)
+        {
+            if (recoilTimer < recoilLength)
+            {
+                recoilTimer += Time.deltaTime;
+            }
+            else
+            {
+                isRecoiling = false;
+                recoilTimer = 0;
+            }
+        }
     }
-
+    public virtual void EnemyHit(float _damageDone, Vector2 _hitDirection, float _hitForce)
+    {
+        health -= _damageDone;
+        if (!isRecoiling)
+        {
+            rb.AddForce(-_hitForce * recoilFactor * _hitDirection);
+        }
+    }
+    protected void OnTriggerStay2D(Collider2D _other)
+    {
+        if (_other.CompareTag("Player") && !PlayerMovement.Instance.pState.invincible)
+        {
+            Attack();
+            PlayerMovement.Instance.HitStopTime(0, 5, 0.5f);
+        }
+    }
+    protected virtual void Attack()
+    {
+        PlayerMovement.Instance.TakeDamage(damage);
+    }
 }
