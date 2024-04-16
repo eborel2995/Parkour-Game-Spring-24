b476b39539d98ae8fc4cc6301725ce3c70680294
MagicalMongoose <drew1681@gmail.com>
2024-04-16 12:29:53 -0500

    Implemented boss fight

--- Assets/Scripts/Enemies/BossScript.cs
+++ Assets/Scripts/Enemies/BossScript.cs
@@ -9,6 +9,12 @@ public class BossScript : MonoBehaviour
     [Header("Idle")]
     [SerializeField] float idleMoveSpeed;
     [SerializeField] Vector2 idleMoveDirection;
+
+    private Rigidbody2D rb;
+
+    private float timeSinceLastJump = 2f;
+    private float jumpForce = 20f;
+    private float moveForce = 10f;
     // For Attack Up and Down Stage
     // For Attack Player Stage
     // Other
@@ -16,12 +22,31 @@ public class BossScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void FixedUpdate()
+    {
+        if (timeSinceLastJump > 3) 
+        {
+
+            Vector3 playerPosition = PlayerMovement.Instance.transform.position;
+            Vector3 myPosition = transform.position;
+            Vector2 direction = (playerPosition - myPosition).normalized;
+
+            //bug: player can sit between moveForce distance and the slime will never hit the player
+            //adjust moveForce based on distance
+
+            rb.velocity = new Vector2(rb.velocity.x + (direction.x * moveForce), rb.velocity.y + jumpForce);
+            timeSinceLastJump = 0;
+        }
+
+        timeSinceLastJump += Time.deltaTime;
     }
 }
