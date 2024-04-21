3692529d5835243bf0efbae644ada2c284208b3a
MagicalMongoose <drew1681@gmail.com>
2024-04-21 11:15:15 -0500

    Renamed SlimeEngulfment to BossSlowsPlayer

--- Assets/Scripts/Enemies/BossSlowsPlayer.cs
+++ Assets/Scripts/Enemies/BossSlowsPlayer.cs
@@ -2,16 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SlimeEngulfment : MonoBehaviour
+public class BossSlowsPlayer : MonoBehaviour
 {
     // Create relevant instances.
     private GameObject player;
     private PlayerStatesList pState;
-
-    // "[SerializeFeild]" allows these variables to be edited in Unity.
-    // Boss variables.
-    private bool Engulfed = false;
-    [SerializeField] private float damageToPlayer = 10f;
 
     // Start() is called before the first frame update.
     void Start()
@@ -21,29 +16,15 @@ public class SlimeEngulfment : MonoBehaviour
         pState = player.GetComponent<PlayerStatesList>();
     }
 
-    // Update() is called once per frame.
-    void Update()
-    {
-        // Update PlayerStatesList script whether or not the player is engulfed.
-        if (Engulfed) 
-        {
-            pState.slowed = true;
-        }
-        else
-        {
-            pState.slowed = false; 
-        }
-    }
-
     // Player takes damage while engulfed.
     private void OnTriggerEnter2D(Collider2D col)
     {
-        Engulfed = true;
+        pState.slowed = true;
     }
 
     // Disable engulfed.
     private void OnTriggerExit2D(Collider2D collision)
     {
-        Engulfed = false;
+        pState.slowed = false;
     }
 }
