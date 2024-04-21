e807dfa434fda274807515747b63bf8bfab826ae
curtlandmcdonald <105223978+curtlandmcdonald@users.noreply.github.com>
2024-04-20 19:02:11 -0500

    Cleaned up ParticleDamage script.

--- Assets/Scripts/Enemies/ParticleDamage.cs
+++ Assets/Scripts/Enemies/ParticleDamage.cs
@@ -4,21 +4,25 @@ using UnityEngine;
 
 public class ParticleDamage : MonoBehaviour
 {
+    // "[SerializeFeild]" allows these variables to be edited in Unity.
+    // Access relevant components.
     private GameObject player;
     private HealthManager healthManager;
     [SerializeField] private float damagePerParticle = 1f;
-    // Start is called before the first frame update
+
+    // Start() is called before the first frame update.
     void Start()
     {
+        // Access components once to save processing power.
         player = GameObject.Find("Player");
         healthManager = player.GetComponent<HealthManager>();
     }
 
+    // Handles particle collision with player.
     private void OnParticleCollision(GameObject other)
     {
         if (other == player)
         {
-            Debug.Log("Player hit by damaging particles!");
             healthManager.TakeDamage(damagePerParticle);
         }
     }
